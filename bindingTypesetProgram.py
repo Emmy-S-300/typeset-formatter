1cf1980ce34c76e8fe5735e2ae88aa77ca5c1212
Emmy Schonau <emmy.schonau@gmail.com>
2025-09-06 13:25:12 -0400

    Added writing the file to a word doc.

--- bindingTypesetProgram.py
+++ bindingTypesetProgram.py
@@ -3,24 +3,38 @@ This program is to take any of my favorite online stories and format
  them into a typset so that I can print them and bind them.
 """
 import sys
-from tkinter import Tk, filedialog
+#from tkinter import Tk, filedialog
+from docx import Document
+from docx.enum.section import WD_ORIENT
+
 
 #I want to import an html file
 def html_file_read(file_path):
     with open(file_path, "r", encoding="utf-8") as file:
         return file.read()
     
+#I want the html file to be written to a word document
+def write_to_word_doc(content, output_path):
+    document = Document() #create a new document
+    document.add_paragraph(content) #add the content to the document
+    document.save(output_path)#save the output in the document
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print("Useage: python your_script.py <html_file>")
+        print("Usage: python your_script.py <html_file>")
 
     else:
         html_path = sys.argv[1]
         html_content = html_file_read(html_path)
-        print(html_content)
+        output_path = 'output.dock'
+        write_to_word_doc(html_content, output_path)
+        print("HTML content has been written to {output_path}")
+
 
 #I want it in landscape
+"""def landscape_mode():
+    section = document.sections[-1]
+    section.orientation = WD_ORIENT.LANDSCAPE"""
 
 #I want a justified alignment
 
