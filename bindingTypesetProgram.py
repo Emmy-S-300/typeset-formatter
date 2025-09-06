5e0c9bbf884f44a7221df368a849d62ceb06f305
Emmy Schonau <emmy.schonau@gmail.com>
2025-09-05 20:05:24 -0400

    The program can accept html files and read them

--- bindingTypesetProgram.py
+++ bindingTypesetProgram.py
@@ -2,5 +2,32 @@
 This program is to take any of my favorite online stories and format
  them into a typset so that I can print them and bind them.
 """
+import sys
+from tkinter import Tk, filedialog
 
-from docx import Document
+#I want to import an html file
+def html_file_read(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
+    
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Useage: python your_script.py <html_file>")
+
+    else:
+        html_path = sys.argv[1]
+        html_content = html_file_read(html_path)
+        print(html_content)
+
+#I want it in landscape
+
+#I want a justified alignment
+
+# Margins of at least half an inch all around 
+
+# font in Garamond
+
+# font size in 11
+
+# save it as a word document
